c41ccfb5c5009e596497c54d40ecea24d2214e23
Jeff Sheffel <jeff@sheffel.org>
2017-07-02 11:38:43 -0600

    phpunit: add test of expected globals definitions for new command line feature

--- php/phpunit/tests/design-patterns/Behavioral/ChainOfResponsibilities/Tests/ChainTest.php
+++ php/phpunit/tests/design-patterns/Behavioral/ChainOfResponsibilities/Tests/ChainTest.php
@@ -22,6 +22,25 @@ class ChainTest extends TestCase
         );
     }
 
+    public function testGlobalsAssignments()
+    {
+        //fwrite(STDERR, print_r( $GLOBALS, true ));    // debug output $GLOBALS array
+
+        if (!isset($GLOBALS['myGlobalFromXml'])) {
+            $GLOBALS['myGlobalFromXml'] = null; // ensure default assignment
+        }
+            
+        // check that phpunit.xml has expected global defined
+        $this->assertNotNull($GLOBALS['myGlobalFromXml'], "Expecting a global in phpunit.xml defined as: myGlobalFromXml");
+
+        if (!isset($GLOBALS['myGlobalFromCommand'])) {
+            $GLOBALS['myGlobalFromCommand'] = null; // ensure default assignment
+        }
+            
+        // check that command line has expected global defined (eg. --global myGlobalFromCommand="foo")
+        $this->assertNotNull($GLOBALS['myGlobalFromCommand'], "Expecting a global defined on command line as: --global myGlobalFromCommand=\"anything\"");
+    }
+
     public function testCanRequestKeyInFastStorage()
     {
         $uri = $this->createMock('Psr\Http\Message\UriInterface');
